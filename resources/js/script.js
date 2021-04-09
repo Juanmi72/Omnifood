926c84d5ecff5ba883f971400cbcad22e8a045ea
Juanmi72 <juanmifer@gmail.com>
2021-04-09 21:45:38 +0200

    Seccion 7 Unidad 66

--- resources/js/script.js
+++ resources/js/script.js
@@ -86,4 +86,63 @@ $('document').ready(function() {
     }, {
         offset: '50%'
     });
+
+    /* Mobile nav */
+    /* $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        /* Metodo que abre una caja 
+        nav.slideToggle(200);
+
+        if (icon.hasClass('.ion-navicon-round')) {
+            icon.addClass('.ion-close-round');
+            icon.removeClass('.ion-navicon-round');
+        } else {
+            icon.addClass('.ion-navicon-round');
+            icon.removeClass('.ion-close-round');
+        }
+    }); */ 
+
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element){
+            //Gets the class name of the element that triggered the event
+            var clicked = element.target.className;
+            //Exits the function if the menu is closed AND the logo-black element (logo image) was clicked
+            if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+                return;
+            //Opens and closes the menu
+            if ($(window).width() < 768){
+                nav.slideToggle(100);
+            }
+            //Changes icon states of the menu button
+            if (icon.hasClass('ion-navicon-round')) {
+                icon.addClass('ion-close-round');
+                icon.removeClass('ion-navicon-round');
+            } else {
+                icon.addClass('ion-navicon-round');
+                icon.removeClass('ion-close-round');
+            }
+        });
+        $(window).resize(function(){
+            if ($(window).width() > 767){
+                nav.css("display", "block");
+                icon.addClass('ion-close-round');
+                icon.removeClass('ion-navicon-round');
+            } else {
+                nav.css("display", "none");
+                icon.addClass('ion-navicon-round');
+                icon.removeClass('ion-close-round');
+            }
+        });
+        nav.slideToggle(100);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        } 
+
 });
+
